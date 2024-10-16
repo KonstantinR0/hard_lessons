5a34ffdf2fe13efed7c451312626e64a41f8e994
KonstantinR0 <kostik-rovin@mail.ru>
2024-10-16 19:42:14 +0500

    It's work!

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ def max_iz_2(a, b):
 def emptiness():
     pass
 
+def name(n):
+    for i in range(0, n + 1, 2):
+        yield i
 
 def test_max_iz_2():
     assert max_iz_2(4, 5), "Молодец, верно"
-    # assert max_iz_2(-1, 0), "ошибка, отрицательное число"
-    # assert max_iz_2(0, 0), "равно" из за этих двух строк выдается ошибка, в чем проблема?
+    assert max_iz_2(-1, 0), "ошибка, отрицательное число"
+    assert max_iz_2(0, 0), "равно"
+    return a > b
 
-result = max_iz_2(5,10)
-test_max_iz_2()
+result = max_iz_2(5, 10)
+result2 = list(name(100))
 
 print(result)
 print("Функция определена, но пока ничего не делает.")
+print(result2)
 print("Все тесты пройдены!")
